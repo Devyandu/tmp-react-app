97884d3e3770c87d448f4e7f6acad4a2d7f4d43c
devyandu <devyandu@gmail.com>
2024-02-18 16:15:36 -0500

    Dummy Page

--- app/locations/[id]/page.tsx
+++ app/locations/[id]/page.tsx
@@ -5,6 +5,11 @@ export default function LocationPage({ params }: { params: { id: string } }) {
 <>
     <h1>Here</h1>
     <p>This i the id {params.id}</p>
+    <p>This i the id {params.id}</p>
+    <p>This i the id {params.id}</p>
+    <p>This i the id {params.id}</p>
+    <p>This i the id {params.id}</p>
+    <p>This i the id {params.id}</p>
     </>
   )
   
